c8fa9ee9968b8bd773561b8ea8638bbceaedb70d
daaniissh <pkdanish329@gmail.com>
2024-10-26 20:21:20 +0530

    feat: updated App.tsx to integrate new authentication flow and UI components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,10 @@ import './App.css'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import Login from './pages/auth/Login.tsx'
+import SignUp from './pages/auth/Signup.tsx'
+import EmailSend from './pages/auth/EmailSend.tsx'
+import OtpVerfication from './pages/auth/OtpVerfication.tsx'
+import ResetPassword from './pages/auth/ResetPassword.tsx'
 function App() {
 const [authUser,setAuthUser] = useState(false)
 
@@ -12,7 +16,10 @@ const [authUser,setAuthUser] = useState(false)
       <Routes>
         {/* <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} /> */}
         <Route path='/login' element={!authUser ? <Login /> : <Navigate to="/" />} />
-        {/* <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} /> */}
+        <Route path='/signup' element={!authUser ? <SignUp /> : <Navigate to="/" />} />
+        <Route path='/emailverfication' element={!authUser ? <EmailSend /> : <Navigate to="/" />} />
+        <Route path='/otpverfication' element={!authUser ? <OtpVerfication /> : <Navigate to="/" />} />
+        <Route path='/resetpassword' element={!authUser ? <ResetPassword /> : <Navigate to="/" />} />
         {/* <Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to="/login" />} />
         <Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} /> */}
       </Routes>
