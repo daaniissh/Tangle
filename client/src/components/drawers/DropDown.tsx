798972ff3012c14858a05e6c6c134b38be38e01f
daaniissh <pkdanish329@gmail.com>
2024-10-28 13:34:23 +0530

    feat: implement drawer components for some comp and drop down

---
+++ client/src/components/drawers/DropDown.tsx
@@ -0,0 +1,107 @@
+import {
+  Bookmark,
+
+  LogOut,
+
+  Moon,
+
+  Settings,
+
+  Sun,
+
+} from "lucide-react"
+
+
+import {
+  DropdownMenu,
+
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+
+  DropdownMenuSeparator,
+
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Label } from "@radix-ui/react-label";
+import { Switch } from "@/components/ui/switch"
+import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
+type DropdownMenuDemoProps = {
+  children: React.ReactNode;
+  showStatusBar: boolean;
+  handleCheckedChange: () => void;
+};
+type ThemeProps = {
+  showStatusBar: boolean;
+  handleCheckedChange: () => void;
+};
+
+
+
+
+
+
+
+export function ThemeSwitch({ handleCheckedChange, showStatusBar }: ThemeProps) {
+
+
+
+  return (
+    <div className="flex items-center  py-1 space-x-2">
+      <Sun id="theme" className="size-4" />
+      <Switch
+        className="scale-75" // Adjust scale to your preference (0.5, 0.75, etc.)
+        checked={showStatusBar}
+        onCheckedChange={handleCheckedChange}
+        id="theme"
+      />
+
+      <Moon id="theme" className="size-4" />
+    </div>
+  );
+
+}
+
+export function MenuDropDown({ children, handleCheckedChange, showStatusBar }: DropdownMenuDemoProps) {
+
+  // const [showActivityBar, setShowActivityBar] = useState<Checked>(false)
+  return (
+    <DropdownMenu >
+      <DropdownMenuTrigger asChild>
+        {children}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56 rounded-xl ml-5 px-4 py-5">
+        {/* <DropdownMenuLabel>More</DropdownMenuLabel> */}
+        {/* <DropdownMenuSeparator /> */}
+        <DropdownMenuGroup  >
+          <DropdownMenuItem  >
+            <Settings />
+            <span>Settings</span>
+
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Bookmark />
+            <span>Saved</span>
+
+          </DropdownMenuItem>
+
+
+        </DropdownMenuGroup>
+
+        <DropdownMenuSeparator />
+
+
+        <div className="p-3" ><ThemeSwitch handleCheckedChange={handleCheckedChange} showStatusBar={showStatusBar} /></div>
+
+
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogOut />
+          <span>Log out</span>
+
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
