f6c75984c5cea67c94176cfd0812f0e127f710bc
daaniissh <pkdanish329@gmail.com>
2024-11-02 23:34:07 +0530

    feat: update Home page components and layout

---
+++ client/src/pages/Home/Post.tsx
@@ -0,0 +1,72 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Heart, MessageCircle, Send, Bookmark } from 'lucide-react'
+import React, { useState } from 'react'
+
+const Post = () => {
+  const [more, setMore] = useState(false)
+  const text = "Somethin’ spooky settlin’ in ‘round these parts this fine Halloween. Locals startin’ to call it some kinda…Undead Nightmare Reckon some tips’ll help ya. Just click on the #RedDeadRedemption link in that there bio"
+
+  return (
+    <div className="w-full md:w-8/12 mx-auto mt-2 dark:border-b   dark:border-gray-600/50 rounded-lg overflow-hidden">
+
+      {/* Post Header */}
+      <div className="flex items-center py-2 px-2 md:px-0">
+        <div className="  bg-gradient-to-tr from-insta-gradientStart via-insta-gradientMid to-insta-gradientEnd p-[1.5px]  rounded-full ">
+          <div className="  bg-white p-[1px]  rounded-full ">
+            <Avatar className="w-8 h-8 cursor-pointer">
+              <AvatarImage
+                className="rounded-full select-none object-cover"
+                src="https://i.pinimg.com/736x/0a/2f/68/0a2f68448ab64c7fb67e75ef410de163.jpg"
+              />
+              <AvatarFallback>DN</AvatarFallback>
+            </Avatar>
+          </div>
+        </div>
+        <div className="ml-2 flex-1">
+          <h1 className="text-sm md:text-[14px] font-[500] cursor-pointer whitespace-nowrap overflow-hidden text-black dark:text-white text-ellipsis">
+            daniish • <span className="font-poppins text-center text-sm md:text-[14px] ml-1 text-gray-400">1h</span>
+          </h1>
+        </div>
+      </div>
+
+      {/* Post Image */}
+      <div className="flex justify-center rounded-[6px] border-[0.5px] border-insta-darkPrimary/20">
+        <img
+          draggable="false"
+          className="w-[1080px] h-auto object-cover md:rounded-[6px]"
+          src="https://i.pinimg.com/564x/cb/7f/2a/cb7f2a7ce16db8ab86fe8612883cbe78.jpg"
+          alt="Post Image"
+        />
+      </div>
+
+
+      {/* Post Actions and Caption */}
+      <div className="px-2 md:px-0 py-2 text-black dark:text-white space-y-2">
+        <div className="flex items-center justify-between space-x-2">
+          <div className="flex gap-4 items-center justify-center">
+            <Heart className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" />
+            <MessageCircle className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" />
+            <Send className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" />
+          </div>
+          <Bookmark className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" />
+        </div>
+
+        <p className="text-xs md:text-sm font-normal">Liked by <span className="font-bold">user123</span> and <span className="font-bold">others</span></p>
+
+        {/* Post Text with Show More */}
+        <div className="flex gap-1 flex-col md:flex-row flex-1">
+          <span className={`font-normal text-sm md:text-[14px] ${more ? 'text-left' : 'text-center whitespace-nowrap overflow-hidden text-ellipsis'} text-black dark:text-white max-w-[95%]`}>
+            <span className="font-semibold text-sm md:text-[15px] mr-1">daniish</span> {text}
+          </span>
+          {!more && text.length > 50 && (
+            <span onClick={() => setMore(true)} className="text-insta-darkPrimary text-sm md:text-[14px] cursor-pointer ml-1">more</span>
+          )}
+        </div>
+
+        <p className="text-xs md:text-sm cursor-pointer text-gray-400">View all 10 comments</p>
+      </div>
+    </div>
+  )
+}
+
+export default Post
---
+++ client/src/pages/Home/StoryPage.tsx
@@ -0,0 +1,148 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Progress } from '@/components/ui/progress';
+import { ChevronLeftCircle, ChevronRightCircle, Heart, Send, Plus } from 'lucide-react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const StoryPage = () => {
+  const [images, setImages] = useState([
+    {
+      url: "https://i.pinimg.com/564x/7b/17/85/7b1785c3af74470b5aac228a142d88f2.jpg",
+      progress: 0, // Initial progress for the first image
+    },
+    {
+      url: "https://i.pinimg.com/736x/ba/40/1b/ba401b5ab0b197f1d38460c1bdeb6887.jpg",
+      progress: 0, // Initial progress for the second image
+    },
+    // Add more image objects here
+  ]);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const duration = 20000; // 20 seconds for each image
+    const interval = 100; // Update every 100 milliseconds
+    const increment = (interval / duration) * 100; // Calculate how much to increment
+
+    const timer = setInterval(() => {
+      setImages((prev) => {
+        const newImages = [...prev];
+
+        const currentImage = newImages[currentIndex];
+
+        if (currentImage.progress >= 100 && currentIndex == 0) {
+
+          clearInterval(timer); // Stop when progress reaches 100%
+          handleNext(); // Move to next image
+          return newImages; // Return unchanged images
+        }
+
+
+        currentImage.progress += increment; // Increment the current image's progress
+        return newImages; // Update state
+      });
+    }, interval);
+
+    return () => clearInterval(timer); // Cleanup on unmount
+  }, [currentIndex]); // Run effect on currentIndex change
+
+  const handleNext = () => {
+    setImages((prevImages) => {
+      const newImages = [...prevImages]; // Create a copy of the current images
+      newImages[currentIndex].progress = 100; // Reset progress of the current image to 0
+      return newImages; // Return the updated images array
+    });
+    setCurrentIndex((prevIndex) => {
+      // Check if we are not at the last image before incrementing
+      if (prevIndex < images.length - 1) {
+        return prevIndex + 1; // Move to the next image
+      }
+      return prevIndex; // Stay on the last image
+    }); // Loop back to the start
+
+  };
+
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) => {
+      // Only decrement if we are not at the first image
+      if (prevIndex > 0) {
+        const newIndex = prevIndex - 1; // Move to the previous image
+        const oldIndex = newIndex + 1;
+
+        setImages((prevImages) => {
+          const newImages = [...prevImages]; // Create a copy of the current images
+          newImages[newIndex].progress = 0; // Reset progress of the new current image to 0
+          return newImages; // Return the updated images array
+        });
+        setImages((prevImages) => {
+          const newImages = [...prevImages]; // Create a copy of the current images
+          newImages[oldIndex].progress = 0; // Reset progress of the new current image to 0
+          return newImages; // Return the updated images array
+        });
+
+        return newIndex; // Update currentIndex to the new index
+      }
+      return prevIndex; // Stay on the first image
+    });
+  };
+
+
+  return (
+    <div className='w-full flex  flex-col relative items-center overflow-hidden bg-black md:bg-[#1a1a1a] h-screen'>
+      <div className="w-full hidden justify-between md:flex items-center h-10">
+        <div className="w-32 py-5 mt-5 px-3 items-start justify-start">
+          <img className="filter invert" src="./Logo.png" alt="" />
+        </div>
+        <Link to="/" className="text-white px-4 mt-5 cursor-pointer">
+          <Plus className='rotate-45 size-10' />
+        </Link>
+      </div>
+      <div className="w-full  max-w-[650px] top-10 mx-auto absolute z-40 flex justify-between  items-center md:px-14 h-full">
+        <ChevronLeftCircle className='md:fill-white/30 md:text-black/0 fill-transparent text-transparent md:h-auto md:w-auto h-screen w-24 transition delay-100 md:hover:fill-white md:hover:text-black cursor-pointer' onClick={handlePrevious} />
+        <ChevronRightCircle className='md:fill-white/30 md:text-black/0 fill-transparent md:h-auto md:w-auto text-transparent  h-[90%] w-24 transition delay-100 md:hover:fill-white md:hover:text-black cursor-pointer' onClick={handleNext} />
+      </div>
+      <div className="flex relative w-full md:w-[400px] h-screen rounded-0 mb-5 md:rounded-[8px]">
+
+
+        <div className="flex justify-between relative flex-col h-full w-full">
+          <div className="flex-1 h-20 flexmd:mt-auto w-full md:w-auto fixed bg-gradient-to-b from-black/30 to-transparent flex-col">
+            <div className="flex justify-center gap-1 mt-1 md:mt-2 w-[400px] md:w-[400px] px-1">
+              {images.map((item) => <Progress value={item.progress} className="bg-[#70716d]/30 w-[380px] h-[2px] mt-2 rounded-[10px]" />)}
+            </div>
+            <div className="mt-2 px-2 flex items-center">
+          
+              <Avatar className="scale-75 duration-150">
+                <AvatarImage
+                  className="rounded-full select-none object-fill"
+                  src={"https://i.pinimg.com/736x/0a/2f/68/0a2f68448ab64c7fb67e75ef410de163.jpg"}
+                />
+                <AvatarFallback>DN</AvatarFallback>
+              </Avatar>
+              <span className="flex flex-row z-50  justify-between w-full items-center">
+                <h1 className="text-[14px] font-[400] cursor-pointer ml-1 whitespace-nowrap overflow-hidden text-white text-ellipsis">
+                  daniish <span className="font-poppins text-center text-[14px] ml-[5px] text-gray-700 shadow">1h</span>
+                </h1>
+                <Link to="/" className="text-white px-4 block md:hidden  cursor-pointer">
+                  <Plus className='rotate-45 size-10' />
+                </Link>
+              </span>
+            </div>
+          </div>
+
+          <div className="w-full h-full flex   mb-14 md:rounded-[8px]">
+            <img className='md:rounded-[8px] w-full md:object-cover object-contain' src={images[currentIndex].url} alt="" />
+          </div>
+
+          <div className="h-20 w-full md:w-[400px] md:bg-gradient-to-t    md:from-black/30 md:to-transparent flex justify-between fixed text-white md:bottom-7 bottom-0">
+            <div className="flex justify-end items-start px-14 md:px-2 py-6 md:py-auto gap-5 w-full h-screen">
+              <Heart className='cursor-pointer fill-insta-gradientMid text-insta-gradientMid' />
+              <Send className='cursor-pointer' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StoryPage;
