de1a33773f8494c97526d67eafc8afac837ae82b
daaniissh <pkdanish329@gmail.com>
2024-11-03 20:01:34 +0530

    chore:changed li to Link for routing

--- client/src/components/common/Li.tsx
+++ client/src/components/common/Li.tsx
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom';
 type LI = {
   onClick: () => void,
   searchOpen: boolean,
@@ -7,22 +8,24 @@ type LI = {
   text: string,
   is_border?: string,
   isNotification: boolean,
-  
-  Icon:React.ComponentType<React.SVGProps<SVGSVGElement>>; 
+  route?: string
+
+  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
 };
 
-const Li = ({onClick,isNotification,searchOpen,Icon ,text,className,is_border}: LI) => {
-
+const Li = ({ onClick, isNotification, searchOpen, Icon, text, className, is_border, route }: LI) => {
+  const location = useLocation();
   return (
-    <li
+    <Link
+      to={route ? route : location.pathname}
       onClick={onClick}
       className={`flex transition duration-200 ${className} dark:hover:bg-insta-darkBorder  gap-3 font-instagram  dark:text-insta-darkText text-[15px] items-center font-medium hover:bg-insta-background rounded-xl cursor-pointer py-2 px-2 group  ${searchOpen || isNotification ? `${is_border} w-auto` : "border-none w-full"
         } `}
     >
       <Icon className="group-hover:scale-110 duration-150" />
       {!searchOpen && !isNotification && text}
-     
-    </li>
+
+    </Link>
   )
 }
 
