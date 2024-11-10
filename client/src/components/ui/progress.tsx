7d8b60b09b1f15a605e6d0ef289adaff37e4271c
daaniissh <pkdanish329@gmail.com>
2024-11-10 14:15:49 +0530

    fix: progress bar

--- client/src/components/ui/progress.tsx
+++ client/src/components/ui/progress.tsx
@@ -10,7 +10,7 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-neutral-100 dark:bg-neutral-800",
+      "relative h-4 w-full overflow-hidden rounded-full bg-neutral-400 dark:bg-neutral-400",
       className
     )}
     {...props}
