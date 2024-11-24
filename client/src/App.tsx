b49e49796b8c368de3b7e81a68ed3b09dce2cdc7
daaniissh <pkdanish329@gmail.com>
2024-11-24 23:19:46 +0530

    feat(App.tsx): integrate new components and adjust routes
    Integrated updated components like PostPage and PostMobilePage. Adjusted routing structure for better user experience.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import Login from './pages/auth/Login.tsx'
 import SignUp from './pages/auth/Signup.tsx'
 import EmailSend from './pages/auth/EmailSend.tsx'
-import OtpVerfication from './pages/auth/OtpVerfication.tsx'
+import OtpVerification from './pages/auth/OtpVerfication.tsx'
 import ResetPassword from './pages/auth/ResetPassword.tsx'
 import SideBar from './components/common/Sidebar.tsx'
 
@@ -14,6 +14,11 @@ import Notification from './pages/main/Notification.tsx'
 import HomePage from './pages/Home/HomePage.tsx'
 import StoryPage from './pages/Home/StoryPage.tsx'
 import Explore from './pages/main/Explore.tsx'
+import Profile from './pages/main/Profile.tsx'
+import ProfilePage from './pages/main/Profile.tsx'
+import ProfileEdit from './pages/main/ProfileEdit.tsx'
+import PostDetails from './components/common/PostDeatils.tsx'
+import PostPage from './pages/main/Postpage.tsx'
 // import { Sidebar } from 'lucide-react'
 
 function App() {
@@ -47,25 +52,31 @@ function App() {
   };
 
   return (
-    <div className='flex   dark:bg-black overflow-hidden  '>
-      {authUser &&  !location.pathname.includes("/story")  && <SideBar showStatusBar={showStatusBar} handleCheckedChange={handleCheckedChange} />}
-       
-        <Routes>
-          <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-          <Route path='/login' element={!authUser ? <Login /> : <Navigate to="/" />} />
-          <Route path='/signup' element={!authUser ? <SignUp /> : <Navigate to="/" />} />
-          <Route path='/emailverfication' element={<EmailSend />} />
-          <Route path='/otpverfication' element={<OtpVerfication />} />
-          <Route path='/story/:username/:id' element={authUser ? <StoryPage /> : <Navigate to="/login" />} />
-          <Route path='/resetpassword' element={<ResetPassword />} />
-          <Route path='/notifications' element={authUser ? <Notification /> : <Navigate to="/login" />} />
-          <Route path='/explore' element={authUser ? <Explore /> : <Navigate to="/login" />} />
-          {/* <Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />  */}
-        </Routes>
-      
-      
-      {/* <Toaster /> */}
+    <div className="flex dark:bg-black overflow-hidden">
+      {authUser && !location.pathname.includes("/story") && (
+        <SideBar showStatusBar={showStatusBar} handleCheckedChange={handleCheckedChange} />
+      )}
+     
+
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+
+        <Route path="/profile/:username" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        <Route path="edit/:username" element={authUser ? <ProfileEdit showStatusBar={showStatusBar} handleCheckedChange={handleCheckedChange} /> : <Navigate to="/login" />} />
+
+        <Route path="/login" element={!authUser ? <Login /> : <Navigate to="/" />} />
+        <Route path="/signup" element={!authUser ? <SignUp /> : <Navigate to="/" />} />
+        <Route path="/emailverification" element={<EmailSend />} />
+        <Route path="/otpverification" element={<OtpVerification />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
+
+        <Route path="/story/:username/:id" element={authUser ? <StoryPage /> : <Navigate to="/login" />} />
+        <Route path="post/:username/:postId" element={<PostPage />} />
+        <Route path="/notifications" element={authUser ? <Notification /> : <Navigate to="/login" />} />
+        <Route path="/explore" element={authUser ? <Explore /> : <Navigate to="/login" />} />
+      </Routes>
     </div>
+
   )
 }
 
