76106c8a8bff4c632ad26bcd4ed7e99cdcc00bea
daaniissh <pkdanish329@gmail.com>
2024-11-24 23:20:39 +0530

    chore(Explore): enhance explore page layout and functionality
    Refined the Explore page with updated layout and enhanced functionality for better user discovery.

--- client/src/pages/main/Explore.tsx
+++ client/src/pages/main/Explore.tsx
@@ -1,52 +1,22 @@
-import ExplorePost from '@/components/common/ExplorePost'
+import ExplorePost from '@/components/common/SmallPost'
 
 
 const Explore = () => {
   return (
-    <div className="flex justify-center scrollbar-thin dark:scrollbar-track-black scrollbar-thumb-white  dark:scrollbar-thumb-gray-800 w-full min-h-screen md:mb-auto mb-20 md:mt-auto mt-0  overflow-y-scroll h-screen ">
-      <div className="   md:px-5  ">
-        <div className="grid gap-1   grid-cols-3  md:grid-rows-8  lg:grid-rows-3 md:mt-8 mt-16 ">
-
-          <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
-          <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
-
-          <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
-          <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
-
-          <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
-          <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
-
-          <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
-          <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
-
-          <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
-          <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
-
-          <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
-          <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
-
-          <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
-          <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
-
-          <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
-          <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
-
-          <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
-          <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
-
-          <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
-          <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
-
-          <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
-          <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
-
-          <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
-          <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
-
-        </div>
-      </div>
+<div className="flex justify-center w-full min-h-screen overflow-y-scroll h-screen scrollbar-thin dark:scrollbar-track-black scrollbar-thumb-white dark:scrollbar-thumb-gray-800 mb-20 md:mb-0">
+  <div className="w-full px-2 mt-10  md:px-5 max-w-5xl">
+    <div className="grid gap-1 grid-cols-3 auto-rows-fr ">
+      <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
+      <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
+    
+      <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
+      <ExplorePost comments='144' img='https://i.pinimg.com/564x/4b/b3/c9/4bb3c97b54b93dcae6580aeadbe54a1d.jpg' likes='12.4k' />
+      <ExplorePost comments='144' img='https://i.pinimg.com/736x/34/ab/7b/34ab7b5a86415959d0585a199392b396.jpg' likes='12.4k' />
     </div>
+  </div>
+</div>
 
+  
   )
 }
 
