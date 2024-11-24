fbd7d50a32b20a5adfbeca913b57ab3ea21129e4
daaniissh <pkdanish329@gmail.com>
2024-11-24 23:20:34 +0530

    refactor(Posts): streamline post rendering logic
    Streamlined post rendering logic for better performance and readability.

--- client/src/pages/Home/Posts.tsx
+++ client/src/pages/Home/Posts.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Post from './Post'
 import PostSkeleton from '@/components/skeletons/PostSkeleton'
+import { PostData } from '@/lib/mock/post'
 
 const Posts = () => {
   return (
     <div className=' md:w-10/12 min-h-screen mb-20 z-0  overflow-x-hidden justify-center flex-col flex ' >
-      <Post/>
-      <Post/>
+      {PostData.map((data)=><Post data={data} />) }
+    
       {/* <PostSkeleton/> */}
     </div>
   )
