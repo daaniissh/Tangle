ba66926b93785fb0d9269c38fc56a1d66c7878b0
daaniissh <pkdanish329@gmail.com>
2024-11-24 23:20:31 +0530

    feat(Post): enhance post component with additional features
    Added new features to the Post component including improved interactions and visual updates.

--- client/src/pages/Home/Post.tsx
+++ client/src/pages/Home/Post.tsx
@@ -1,8 +1,13 @@
+import PostDetails from '@/components/common/PostDeatils'
+import UserAvatar from '@/components/common/UserAvatar'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/Avatar'
+import { type Post } from '@/lib/mock/post'
 import { Heart, MessageCircle, Send, Bookmark } from 'lucide-react'
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const Post = () => {
+const Post = ({data}:any) => {
+  const {comments,content,isLiked,isSaved,isStory,likesCount,postImg,profileImg,timeAgo,username,stories,id}:Post = data
   const [more, setMore] = useState(false)
   const text = "Somethin’ spooky settlin’ in ‘round these parts this fine Halloween. Locals startin’ to call it some kinda…Undead Nightmare Reckon some tips’ll help ya. Just click on the #RedDeadRedemption link in that there bio"
 
@@ -11,31 +16,17 @@ const Post = () => {
 
       {/* Post Header */}
       <div className="flex items-center py-3 md:py-2 px-3 md:px-0">
-        <div className="  bg-gradient-to-tr from-insta-gradientStart via-insta-gradientMid to-insta-gradientEnd p-[1.5px]  rounded-full ">
-          <div className="  bg-white p-[1px]  rounded-full ">
-            <Avatar className="w-8 h-8 cursor-pointer">
-              <AvatarImage
-                className="rounded-full select-none object-cover"
-                src="https://i.pinimg.com/736x/0a/2f/68/0a2f68448ab64c7fb67e75ef410de163.jpg"
-              />
-              <AvatarFallback>DN</AvatarFallback>
-            </Avatar>
-          </div>
-        </div>
-        <div className="ml-2 flex-1">
-          <h1 className="text-sm md:text-[14px] font-[500] cursor-pointer whitespace-nowrap overflow-hidden text-black dark:text-white text-ellipsis">
-            daniish • <span className="font-poppins text-center text-sm md:text-[14px] ml-1 text-gray-400">1h</span>
-          </h1>
-        </div>
+      <UserAvatar className="w-9 h-9" createdAt={timeAgo} image={profileImg} storyId={id} isStory={isStory} username={username} />
+       
       </div>
 
       {/* Post Image */}
       <div className="flex justify-center rounded-[6px] z-0 border-[0.5px] border-insta-darkPrimary/20">
         <img
           draggable="false"
-          className="w-[1080px] h-auto object-cover rounded-[2px] md:rounded-[6px]"
-          src="https://i.pinimg.com/736x/4e/c5/f8/4ec5f8be7802fd9676c341f6cbab1d8d.jpg"
-          alt="Post Image"
+          className="w-full h-auto object-cover rounded-[2px] md:rounded-[6px]"
+          src={postImg}
+          alt={content}
         />
       </div>
 
@@ -44,26 +35,26 @@ const Post = () => {
       <div className="px-2 md:px-0 py-2 text-black dark:text-white space-y-2">
         <div className="flex items-center justify-between space-x-2">
           <div className="flex gap-4 items-center justify-center">
-            <Heart className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" />
-            <MessageCircle className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" />
+            <Heart  className={`${isLiked && "fill-red-700 text-red-700"} w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary`} />
+          <Link to={`/post/${username}/${id}`} ><MessageCircle className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" /></Link>  
             <Send className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" />
           </div>
-          <Bookmark className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" />
+          <Bookmark className={`${isSaved && "fill-white text-white"} w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary`} />
         </div>
 
-        <p className="text-xs md:text-sm font-normal">Liked by <span className="font-bold">user123</span> and <span className="font-bold">others</span></p>
+        <p className="text-xs md:text-sm font-bold">{likesCount} Likes</p>
 
         {/* Post Text with Show More */}
-        <div className="flex gap-1 flex-col md:flex-row flex-1">
+        <div className="flex gap-1  flex-col md:flex-row flex-1">
           <span className={`font-normal text-sm md:text-[14px] ${more ? 'text-left' : 'text-center whitespace-nowrap overflow-hidden text-ellipsis'} text-black dark:text-white max-w-[95%]`}>
-            <span className="font-semibold text-sm md:text-[15px] mr-1">daniish</span> {text}
+            <span className="font-semibold text-sm md:text-[15px] mr-1">{username}</span> {content}
           </span>
           {!more && text.length > 50 && (
-            <span onClick={() => setMore(true)} className="text-insta-darkPrimary text-sm md:text-[14px] cursor-pointer ml-1">more</span>
+            <span onClick={() => setMore(true)} className="text-insta-darkPrimary dark:text-gray-400 hidden md:block text-sm md:text-[14px] cursor-pointer ml-1">more</span>
           )}
         </div>
 
-        <p className="text-xs md:text-sm cursor-pointer text-gray-400">View all 10 comments</p>
+        <Link to={`/post/${username}/${id}`}><p className="text-xs md:text-sm cursor-pointer text-gray-400 ">View all {comments.length} comments</p></Link>
       </div>
     </div>
   )
