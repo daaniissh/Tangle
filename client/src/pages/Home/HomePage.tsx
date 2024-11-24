cf4954e0f928d013eeaab93af91bafa95ca8dc00
daaniissh <pkdanish329@gmail.com>
2024-11-24 23:20:28 +0530

    refactor(HomePage): optimize post loading and layout
    Improved post loading mechanisms and layout in HomePage for a more efficient and visually pleasing experience.

--- client/src/pages/Home/HomePage.tsx
+++ client/src/pages/Home/HomePage.tsx
@@ -36,12 +36,12 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="flex justify-between  scrollbar-thin dark:scrollbar-track-black scrollbar-thumb-white  dark:scrollbar-thumb-gray-800  z-0 h-screen w-full overflow-x- md:px-2  overflow-y-auto">
+    <div className="flex justify-between   scrollbar-thin dark:scrollbar-track-black scrollbar-thumb-white  dark:scrollbar-thumb-stone-800  z-0 h-screen w-full overflow-x- md:px-2  overflow-y-auto">
 
       {/* Left Section: Stories + Posts */}
       <div className="flex flex-col mt-14 md:mt-0   w-full max-w-[850px] ">
         {/* Scrollable Stories Section */}
-        <div className="relative md:-0 md:mx-5 -b -insta-darkPrimary/50 flex md:justify-center  w-full mt-4">
+        <div className="relative z-0 md:-0 md:mx-5 -b -insta-darkPrimary/50 flex md:justify-center  w-full mt-4">
 
           <div className="absolute w-10/12 hidden px-8 bottom-2 md:flex justify-between items-center h-full  ">
             <ChevronLeftCircle
@@ -56,7 +56,7 @@ const HomePage = () => {
           </div>
           <div
             ref={scrollableDivRef}
-            className="flex relative px-1 md:px-auto overflow-x-auto max-w-[650px] scrollbar-hide md:gap-5 gap-1 py-[1px]  md:py-2"
+            className="flex relative  px-1 md:px-auto overflow-x-hidden max-w-[650px] scrollbar-hide md:gap-5 gap-1 py-[1px]  md:py-2"
           >
 
 
@@ -75,6 +75,7 @@ const HomePage = () => {
 
         {/* Posts Section */}
         <div className="w-full  flex justify-center  ">
+          
           <Posts />
         </div>
       </div>
