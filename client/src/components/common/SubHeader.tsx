3c8bc5316d208784f2653e2edcd6a800cb213cf9
daaniissh <pkdanish329@gmail.com>
2024-11-24 23:20:09 +0530

    feat(SubHeader): update subheader for enhanced navigation
    Enhanced navigation options in SubHeader component for improved accessibility and UX.

--- client/src/components/common/SubHeader.tsx
+++ client/src/components/common/SubHeader.tsx
@@ -4,17 +4,22 @@ import { Link } from 'react-router-dom'
 
 type p = {
   name: string;
-  onClick: (link: string) => void;
+  onClick?: (link: string) => void;
 }
 
 const SubHeader = ({ name, onClick }: p) => {
+  function func(){
+    if(onClick){
+      onClick('/')
+    }
+  }
   return (
-    <div className="border-b-[1px] bg-white dark:bg-black dark:border-insta-darkBorder flex gap-2 border-insta-border w-full items-center justify-center h-16 fixed top-0 z-[9999]">
+    <div className="border-b-[1px] bg-white dark:bg-black dark:border-insta-darkBorder flex gap-2 border-insta-border w-full items-center justify-center h-10 fixed top-0 z-[9999]">
       <div className="flex w-full flex-[1,2] items-center text-white">
         {/* Left-aligned Link element */}
         <Link
           to="/"
-          onClick={() => onClick("/")}
+          onClick={func}
           className="flex dark:hover:bg-insta-darkBorder gap-3 font-instagram text-[15px] dark:text-insta-darkText font-medium hover:bg-insta-background rounded-xl cursor-pointer py-2 px-2 group"
         >
           <ArrowLeft className="group-hover:scale-110 duration-150 text-black dark:text-white" />
