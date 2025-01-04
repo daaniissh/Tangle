22e749c76acaadd21063cd6c888664d8adaa37ad
daaniissh <pkdanish329@gmail.com>
2025-01-04 19:33:48 +0530

    feat: added API

--- client/src/pages/Home/Posts.tsx
+++ client/src/pages/Home/Posts.tsx
@@ -2,12 +2,21 @@ import React from 'react'
 import Post from './Post'
 import PostSkeleton from '@/components/skeletons/PostSkeleton'
 import { PostData } from '@/lib/mock/post'
+import { useQuery } from '@tanstack/react-query'
+import { AuthUser, PostDetails } from '@/types/QueryTypes/queary'
+import { QueryKey } from '@/types/QueryKey/key'
 
 const Posts = () => {
+  const { data: allPosts,isLoading, isPending } = useQuery<AuthUser>({ queryKey: ["posts"] as QueryKey });
+
+ const Posts = Array.isArray(allPosts) ? allPosts : [];
+ if(isLoading || isPending){
+  return <PostSkeleton/>
+ }
   return (
     <div className=' md:w-10/12 min-h-screen mb-20 z-0  overflow-x-hidden justify-center flex-col flex ' >
-      {PostData.map((data)=><Post data={data} />) }
-    
+      {Posts.map((data:PostDetails) => <Post data={data} />)}
+
       {/* <PostSkeleton/> */}
     </div>
   )
