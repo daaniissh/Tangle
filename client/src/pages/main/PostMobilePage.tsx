adabeffa72f6b34ff5dca082b085959b9ffb8f5d
daaniissh <pkdanish329@gmail.com>
2025-01-04 19:33:14 +0530

    feat: added api

--- client/src/pages/main/PostMobilePage.tsx
+++ client/src/pages/main/PostMobilePage.tsx
@@ -1,10 +1,14 @@
 
 import Comment from '@/components/common/Comment'
 import UserAvatar from '@/components/common/UserAvatar'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/Avatar'
+import PostDetailsSkeleton from '@/components/skeletons/PostDetailSkelton'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/Button'
 import { Input } from '@/components/ui/Input'
 import { Post, PostData } from '@/lib/mock/post'
+import { QueryKey } from '@/types/QueryKey/key'
+import { AuthUser, PostDetails } from '@/types/QueryTypes/queary'
+import { useQuery } from '@tanstack/react-query'
 import { Bookmark, Heart, MessageCircle, Send } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
@@ -13,21 +17,63 @@ interface PostDetailsProps {
   username?: string;
   postId?: string;
 }
-const PostMobilePage = ({postId}:PostDetailsProps) => {
-  const postData = PostData.find((data) => data.id === Number(postId))
-  const {comments,content,id,isLiked,isSaved,isStory,likesCount,postImg,profileImg,timeAgo,username,stories}:Post= postData
+const PostMobilePage = ({postId}:any) => {
+
+const APIURL = import.meta.env.VITE_API_URL;
+
+  // const { username, postId } = useParams<{ username: string; postId: string }>();
+
+
+
+  console.log(postId, "====postID")
+  const { data: authUser } = useQuery<AuthUser>({ queryKey: ["authUser"] });
+
+  // const { data: post } = useQuery<AuthUser>({ queryKey: ["posts"] });
+
+
+  const { data: postData, isLoading } = useQuery<PostDetails>({
+    queryKey: ["posts"] as QueryKey,
+    queryFn: async () => {
+      try {
+        const res = await fetch(`${APIURL}/posts/post/${postId}`, {
+          method: 'GET',
+          credentials: 'include',
+        })
+        const data = await res.json()
+        if (data.error) return null
+        if (!res.ok || data.error) {
+          throw new Error(data.error || "Something went wrong")
+        }
+        console.log(data, "===user data")
+        return data
+      } catch (error) {
+
+      }
+    },
+    enabled: !!postId,
+  })
+
+  const isLiked = postData?.likes?.includes(authUser?._id);
+  // if(isLoading){
+  //   return (
+  //     <div className='w-full bottom-0 absolute h-screen' >
+  //        <PostDetailsSkeleton/>
+  //     </div>
+  //   )
+  // }
+
   return (
     <div className="w-full   mx-auto max-h-screen mt-14 overflow-y-scroll md:dark:border-b md:dark:border-gray-600/50 rounded-lg overflow-hidden">
       {/* Post Header */}
-      <UserAvatar image={profileImg} className='' padding='py-4 px-1' createdAt={timeAgo} isStory={isStory} username={username} storyId={id} />
+      <UserAvatar image={postData?.user?.profileImg} className='' padding='py-4 px-1' createdAt={postData?.createdAt} isStory={postData?.user?.is_story} username={postData?.user?.username} storyId={postData?._id} />
 
       {/* Post Image */}
       <div className="flex justify-center rounded-[6px] z-0 border-[0.5px] border-insta-darkPrimary/20">
         <img
           draggable="false"
           className="w-full h-auto object-cover rounded-[2px] md:rounded-[6px]"
-          src={postImg}
-          alt={content}
+          src={postData?.img}
+          alt={postData?.text}
         />
       </div>
 
@@ -39,24 +85,24 @@ const PostMobilePage = ({postId}:PostDetailsProps) => {
             <MessageCircle className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" />
             <Send className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" />
           </div>
-          <Bookmark className={`${postData?.isSaved && "fill-white text-white"} w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary`}/>
+          <Bookmark className={`${postData?.is_save && "fill-white text-white"} w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary`}/>
         </div>
 
-        <p className="text-xs md:text-sm font-bold">{likesCount} Likes</p>
-        <p className="text-xs text-gray-400" >{timeAgo}</p>
+        <p className="text-xs md:text-sm font-bold">{postData?.likes?.length} Likes</p>
+        <p className="text-xs text-gray-400" >{postData?.createdAt}</p>
 
         {/* Post Text with Show More */}
         <div>
           <div className="flex gap-1 flex-col md:flex-row w-full">
             <span className="font-normal text-sm md:text-[14px] text-left whitespace-wrap overflow-hidden text-ellipsis text-black dark:text-white">
-            {content}
+            {postData?.text}
             </span>
           </div>
         </div>
 
         {/* Comment Section */}
         <div className="py-4 border-t flex flex-col gap-5 flex-1 dark:border-gray-800 scrollbar-none min-h-screen pb-40">
-        {postData?.comments.map((data)=><Comment data={data}  />)}
+        {postData?.comments?.map((data)=><Comment data={data}  />)}
         </div>
 
         <div className="w-full absolute bottom-10 px-2  right-0 bg-white  dark:bg-black py-5 flex">
