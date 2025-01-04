792ee93b3bf38f5681beb2280abfa7783820047e
daaniissh <pkdanish329@gmail.com>
2025-01-04 23:32:20 +0530

    feat: added mobile-specific layout improvements to PostMobilePage

--- client/src/pages/main/PostMobilePage.tsx
+++ client/src/pages/main/PostMobilePage.tsx
@@ -8,19 +8,20 @@ import { Input } from '@/components/ui/Input'
 import { Post, PostData } from '@/lib/mock/post'
 import { QueryKey } from '@/types/QueryKey/key'
 import { AuthUser, PostDetails } from '@/types/QueryTypes/queary'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { Bookmark, Heart, MessageCircle, Send } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 interface PostDetailsProps {
- 
+
   username?: string;
   postId?: string;
 }
-const PostMobilePage = ({postId}:any) => {
+const PostMobilePage = ({ postId }: any) => {
 
-const APIURL = import.meta.env.VITE_API_URL;
+  const APIURL = import.meta.env.VITE_API_URL;
 
+  const queryClient = useQueryClient();
   // const { username, postId } = useParams<{ username: string; postId: string }>();
 
 
@@ -31,7 +32,7 @@ const APIURL = import.meta.env.VITE_API_URL;
   // const { data: post } = useQuery<AuthUser>({ queryKey: ["posts"] });
 
 
-  const { data: postData, isLoading } = useQuery<PostDetails>({
+  const { data: postData, isLoading, refetch } = useQuery<PostDetails>({
     queryKey: ["posts"] as QueryKey,
     queryFn: async () => {
       try {
@@ -54,19 +55,55 @@ const APIURL = import.meta.env.VITE_API_URL;
   })
 
   const isLiked = postData?.likes?.includes(authUser?._id);
-  // if(isLoading){
-  //   return (
-  //     <div className='w-full bottom-0 absolute h-screen' >
-  //        <PostDetailsSkeleton/>
-  //     </div>
-  //   )
-  // }
+
+  const { mutate: likePost, isPending: isLiking } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`${APIURL}/posts/like/${postId}`, {
+          method: 'POST',
+          credentials: 'include',
+        })
+        const data = await res.json()
+        if (data.error) return null
+        if (!res.ok || data.error) {
+          throw new Error(data.error || "Something went wrong")
+        }
+        console.log(data, "===user data")
+        return data
+      } catch (error) {
+
+      }
+    }
+    ,
+    onSuccess: (updatedLikes) => {
+      console.log("Liked post", updatedLikes);
+      // instead, update the cache directly for that post
+      queryClient.setQueryData(["posts"], (oldData: any) => {
+        return oldData.map((p: any) => {
+          if (p._id === postId) {
+            return { ...p, likes: updatedLikes };
+          }
+          return p;
+        });
+      });
+    },
+  })
+
+  const postOwner = postData?.user;
+
+  const handleLikePost = () => {
+    if (isLiking) return;
+    likePost();
+    refetch()
+  };
 
   return (
     <div className="w-full   mx-auto max-h-screen mt-14 overflow-y-scroll md:dark:border-b md:dark:border-gray-600/50 rounded-lg overflow-hidden">
       {/* Post Header */}
-      <UserAvatar image={postData?.user?.profileImg} className='' padding='py-4 px-1' createdAt={postData?.createdAt} isStory={postData?.user?.is_story} username={postData?.user?.username} storyId={postData?._id} />
-
+      <div className="flex items-center justify-between px-2 py-1 border-b dark:border-gray-800">
+        <UserAvatar className="w-10 h-10" createdAt={postData?.createdAt} image={postData?.user?.profileImg} storyId={postData?._id} isStory={postData?.user?.is_story} username={postData?.user?.username} />
+        {postOwner && <span className="text-sm text-red-500 font-bold cursor-pointer hover:text-red-900/50 dark:hover:text-red-500/60" >Delete</span>}
+      </div>
       {/* Post Image */}
       <div className="flex justify-center rounded-[6px] z-0 border-[0.5px] border-insta-darkPrimary/20">
         <img
@@ -81,11 +118,11 @@ const APIURL = import.meta.env.VITE_API_URL;
       <div className="px-2 md:px-0 py-2 text-black dark:text-white space-y-2">
         <div className="flex items-center justify-between space-x-2">
           <div className="flex gap-4 items-center">
-          <Heart  className={`${isLiked && "fill-red-700 text-red-700"} w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary`}/>
+            <Heart onClick={handleLikePost} className={`${isLiked && "fill-red-700 text-red-700"} w-5 h-5 md:w-6 md:h-6 cursor-pointer `} />
             <MessageCircle className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" />
             <Send className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" />
           </div>
-          <Bookmark className={`${postData?.is_save && "fill-white text-white"} w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary`}/>
+          <Bookmark className={`${postData?.is_save && "fill-white text-white"} w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary`} />
         </div>
 
         <p className="text-xs md:text-sm font-bold">{postData?.likes?.length} Likes</p>
@@ -95,14 +132,14 @@ const APIURL = import.meta.env.VITE_API_URL;
         <div>
           <div className="flex gap-1 flex-col md:flex-row w-full">
             <span className="font-normal text-sm md:text-[14px] text-left whitespace-wrap overflow-hidden text-ellipsis text-black dark:text-white">
-            {postData?.text}
+              {postData?.text}
             </span>
           </div>
         </div>
 
         {/* Comment Section */}
         <div className="py-4 border-t flex flex-col gap-5 flex-1 dark:border-gray-800 scrollbar-none min-h-screen pb-40">
-        {postData?.comments?.map((data)=><Comment data={data}  />)}
+          {postData?.comments?.map((data) => <Comment data={data} />)}
         </div>
 
         <div className="w-full absolute bottom-10 px-2  right-0 bg-white  dark:bg-black py-5 flex">
