19b21e85b3a8b1ef7cdef357f4de70dff69d5f4d
daaniissh <pkdanish329@gmail.com>
2025-01-04 19:28:38 +0530

    fix: implemented API

--- client/src/components/common/PostDeatils.tsx
+++ client/src/components/common/PostDeatils.tsx
@@ -4,9 +4,15 @@ import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 import { Bookmark, Heart, MessageCircle, Send, X } from "lucide-react";
 import Comment from "./Comment";
-import {  PostData } from "@/lib/mock/post";
+
 import UserAvatar from "./UserAvatar";
 import ShareDialog from "./Share";
+import { useQuery } from "@tanstack/react-query";
+import { QueryKey } from "@/types/QueryKey/key";
+import { AuthUser, PostDetails as post } from "@/types/QueryTypes/queary";
+import PostSkeleton from "../skeletons/PostSkeleton";
+import PostDetailsSkeleton from "../skeletons/PostDetailSkelton";
+import { Skeleton } from "../ui/Skeleton";
 
 interface PostDetailsProps {
   children?: ReactNode;
@@ -17,13 +23,49 @@ interface PostDetailsProps {
 }
 
 const PostDetails: React.FC<PostDetailsProps> = ({ children, isDialogOpen, onClose, postId }) => {
-  const postData = PostData.find((data) => data.id === Number(postId))
+
 
   if (!isDialogOpen) return null; // Don't render if the dialog is not open
+  const APIURL = import.meta.env.VITE_API_URL;
+
+  // const { username, postId } = useParams<{ username: string; postId: string }>();
+
+
+
+  console.log(postId, "====postID")
+  const { data: authUser } = useQuery<AuthUser>({ queryKey: ["authUser"] });
+
+  // const { data: post } = useQuery<AuthUser>({ queryKey: ["posts"] });
+
+
+  const { data: Post, isLoading } = useQuery<post>({
+    queryKey: ["posts"] as QueryKey,
+    queryFn: async () => {
+      try {
+        const res = await fetch(`${APIURL}/posts/post/${postId}`, {
+          method: 'GET',
+          credentials: 'include',
+        })
+        const data = await res.json()
+        if (data.error) return null
+        if (!res.ok || data.error) {
+          throw new Error(data.error || "Something went wrong")
+        }
+        console.log(data, "===user data")
+        return data
+      } catch (error) {
+
+      }
+    },
+    enabled: !!postId,
+  })
+
+  const isLiked = Post?.likes?.includes(authUser?._id);
 
   return (
     <div className="bg-gray-100 dark:bg-black dark:text-white">
       {/* Render Children */}
+
       <div
         onClick={onClose} // Close dialog when clicking children
         className="cursor-pointer bg-white dark:bg-black"
@@ -45,33 +87,40 @@ const PostDetails: React.FC<PostDetailsProps> = ({ children, isDialogOpen, onClo
             onClick={(e) => e.stopPropagation()} // Prevent click propagation inside dialog
           >
             {/* Left: Image */}
+            {isLoading ? 
+            <div className="w-full  bg-black">
+              <Skeleton className="h-full w-full object-cover" />
+            </div> 
+            : 
             <div className="w-full bg-black">
               <img
-                src={postData?.postImg}
+                src={Post?.img || "https://i.pinimg.com/736x/d3/14/d7/d314d796a18a4f4e37fa94a0314c5762.jpg"}
                 alt="Post Content"
                 className="h-full w-full object-cover"
               />
-            </div>
+            </div>}
 
             {/* Right: Content */}
             <div className="flex flex-col w-full">
               {/* Post Header */}
-              <div className="flex items-center justify-between px-2 py-1 border-b dark:border-gray-800">
-               <UserAvatar className="w-10 h-10" createdAt={postData?.timeAgo} image={postData?.profileImg} storyId={postData?.id} isStory={postData?.isStory} username={postData?.username} />
-              </div>
+           {isLoading ? <div className="flex items-center  justify-between px-2 py-1 border-b dark:border-gray-800">
+          <Skeleton className="w-10 h-10 rounded-full" />
+        </div> :   <div className="flex items-center justify-between px-2 py-1 border-b dark:border-gray-800">
+                <UserAvatar className="w-10 h-10" createdAt={Post?.createdAt} image={Post?.user?.profileImg} storyId={Post?._id} isStory={authUser?.is_story} username={Post?.user?.username} />
+              </div>}
 
               {/* Post Description */}
-              <div className="p-4">
+             {Post?.text && <div className="p-4">
                 <div className="flex gap-1 flex-col md:flex-row flex-1 w-full max-w-full">
                   <span className="font-normal text-sm md:text-[14px] text-center md:text-left whitespace-wrap overflow-hidden text-ellipsis text-black dark:text-white block w-full">
-                    {postData?.content}
+                    {Post?.text}
                   </span>
                 </div>
-              </div>
+              </div>}
 
               {/* Comments Section */}
               <div className="p-4 border-t flex flex-col gap-4 scrollbar-none dark:border-gray-800 overflow-y-auto flex-1">
-                {postData?.comments.map((data)=><Comment data={data}  />)}
+                {Post?.comments?.map((data) => <Comment data={data} />)}
                 {/* Repeat as needed */}
               </div>
 
@@ -80,15 +129,15 @@ const PostDetails: React.FC<PostDetailsProps> = ({ children, isDialogOpen, onClo
                 <div className="px-2 w-full md:px-0 py-2 text-black dark:text-white space-y-2">
                   <div className="flex items-center justify-between space-x-2">
                     <div className="flex gap-4 items-center justify-center">
-                      <Heart  className={`${postData?.isLiked && "fill-red-700 text-red-700"} w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary`}/>
+                      <Heart className={`${isLiked && "fill-red-700 text-red-700"} w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary`} />
                       <MessageCircle className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" />
-                     <ShareDialog id={postData?.id} username={postData?.username} ><button> <Send className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" /></button></ShareDialog>
+                      <ShareDialog id={Post?._id} username={Post?.user?.username} ><button> <Send className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary" /></button></ShareDialog>
                     </div>
-                    <Bookmark className={`${postData?.isSaved && "fill-white text-white"} w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary`}/>
+                    <Bookmark className={`${Post?.is_save && "fill-white text-white"} w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:text-insta-darkPrimary`} />
                   </div>
 
-                  <p className="text-xs md:text-sm font-bold">{postData?.likesCount} Likes</p>
-                  <p className="text-xs text-gray-400" >{postData?.timeAgo}</p>
+                  <p className="text-xs md:text-sm font-bold">{Post?.likes?.length} Likes</p>
+                  <p className="text-xs text-gray-400" >{Post?.createdAt}</p>
                 </div>
                 <div className="w-full border-t dark:border-gray-900/90 py-3 flex">
                   <Input
