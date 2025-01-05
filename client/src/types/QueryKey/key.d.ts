c36810c8fa20c74ef1464a3855ea6223160278c9
daaniissh <pkdanish329@gmail.com>
2025-01-06 00:36:56 +0530

    fix(types): updated query key types and improved typings
    Corrected type definitions in key.d.ts.

--- client/src/types/QueryKey/key.d.ts
+++ client/src/types/QueryKey/key.d.ts
@@ -2,5 +2,6 @@ export type QueryKey =
   | readonly ["authUser"]
   | readonly ["search", string]
   | readonly ["posts",string]
+  | readonly ["following"]
   | readonly ["notifications"]
   | readonly ["users"];
