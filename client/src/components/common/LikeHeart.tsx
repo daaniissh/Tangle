722159455e4c96fd0eac0af40db4ce31ca7a8178
daaniissh <pkdanish329@gmail.com>
2025-01-06 00:36:56 +0530

    feat(like): added LikeHeart component for post animations
    Implemented animated like heart effect on double-tap.

---
+++ client/src/components/common/LikeHeart.tsx
@@ -0,0 +1,12 @@
+import { Heart } from 'lucide-react'
+import React from 'react'
+
+const LikeHeart = () => {
+  return (
+    <div className="absolute select-none top-0 m-auto flex justify-center animate-shakeAndHide items-center h-full w-full">
+      <Heart className="size-36 fill-rose-600 text-rose-600" />
+    </div>
+    )
+}
+
+export default LikeHeart
